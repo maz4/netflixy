5d8542640a5964bcdae6280a382e437b852f7e2c
Marcin <zasadzkimarcin@gmail.com>
2019-08-13 07:56:16 +0100

    create root component, fix issue with dev tools

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -4,7 +4,7 @@ import DockMonitor from 'redux-devtools-dock-monitor';
 import LogMonitor from 'redux-devtools-log-monitor';
 
 export default createDevTools(
-    <DockMonitor toggleVisibilityKey="ctr-h" changePositionKey="ctr-q">
+    <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
         <LogMonitor />
     </DockMonitor>
 );
