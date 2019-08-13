15737f66da6804d69c73522d143db0fc8d617aba
Marcin <zasadzkimarcin@gmail.com>
2019-08-13 06:23:50 +0100

    prepare redux dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import logger from 'redux-logger';
 import api from '../middleware/api';
 import rootReducer from '../reducers/index';
+import DevTools from '../containers/DevTools';
 
 const configureStore = initialState => {
-    const store = createStore(rootReducer, initialState applyMiddleware(logger, api));
+    const store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(logger, api),
+            DevTools.instrument()
+            )
+        );
 
-    if(module.hot){
+    if (module.hot) {
         //Enable webpack hot module replacement for reducers
         module.hot.accept('../reducers', () => {
             store.replaceReducer(rootReducer)
