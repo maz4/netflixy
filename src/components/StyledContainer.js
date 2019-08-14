6eeff8e50e3ccb28f012c6cc7689b475417f780b
Marcin <zasadzkimarcin@gmail.com>
2019-08-14 06:49:08 +0100

    add global styling

---
+++ src/components/StyledContainer.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background: #333852;
+    
+    &:before {
+    content: "NETFLIXY";
+    position: fixed;
+    top: 18vh;
+    left: 10vw;
+    font-size: 9vw;
+    color: rgba(255, 255, 255, 0.03);
+    }
+`;
+
+export default StyledContainer;
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../store';
 import DevTools from './DevTools';
+import StyledContainer from '../components/StyledContainer';
 
 const Root = () => {
     return (
         <Provider store={store}>
-            <div>
-                <h1>Hello, Root Component</h1>
-                <DevTools />
-            </div>
+            <StyledContainer>
+                {/*<DevTools />*/}
+            </StyledContainer>
         </Provider>
     );
 };
