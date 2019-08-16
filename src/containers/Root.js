d54e168f1ecf743ed6533a31280ff38f031c22c3
Marcin <zasadzkimarcin@gmail.com>
2019-08-16 07:26:58 +0100

    add react router dom

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components'
 import store from '../store';
 import DevTools from './DevTools';
@@ -12,8 +13,10 @@ const Root = () => {
         <Provider store={store}>
             <ThemeProvider theme={theme}>
                 <StyledContainer>
-                    <App />
-                    <DevTools />
+                    <Router>
+                        <App/>
+                        {/*<DevTools/>*/}
+                    </Router>
                 </StyledContainer>
             </ThemeProvider>
         </Provider>
