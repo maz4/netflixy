2b7692cda97f64b2376697d4157d2330cf785085
Marcin <zasadzkimarcin@gmail.com>
2019-08-17 17:39:50 +0100

    build footer and footer button components

---
+++ src/components/StyledLargeBtn.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { devices } from '../utils/styledUtils';
+
+const StyledLargeBtn = styled.button`
+    display: block;
+    width: 100%;
+    height: 50px;
+    margin: 0 auto;
+    border: 0;
+    border-radius: 5px;
+    color: #fff;
+    background: #eb6259;
+    transition: all 0.1s cubic-bezier(0.67, 0.13, 0.1, 0.81);
+
+    &:after{
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+        transform: scaleY(0);
+        background: #ac4743;
+        transition: transform 1s cubic-bezier(0.19, 1, 0.22, 1);
+    }
+
+    &:hover{
+        cursor: pointer;
+        transform: translateY(2px);
+        box-shadow: 10px 55px 150px -20px #2a628f;
+        
+        &:after {
+            transform: scaleY(1);
+            transform-origin: bottom;
+        }
+    }
+
+    &:active{
+        transform: translateY(4px);
+    }
+
+    ${devices.md`
+        width: 60%;
+        position: relative;
+        top: 80%;
+    `};
+`;
+
+
+export default StyledLargeBtn;
