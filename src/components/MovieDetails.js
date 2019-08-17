277f837e4608f6c1bba3974c6479b924fc3c718e
Marcin <zasadzkimarcin@gmail.com>
2019-08-17 15:39:50 +0100

    build header comonents

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import StyledHeader from './StyledHeader';
+import FontAwesomeIcon from './FontAwesomeIcon';
+import StyledHeaderLink from './StyledHeaderLink';
 
 const MovieDetails = () => {
     return(
-        <div>Movie Details</div>
+        <StyledHeader>
+            <StyledHeaderLink href="/">
+                <FontAwesomeIcon icon="chevron-left" text="Go back" />
+            </StyledHeaderLink>
+        </StyledHeader>
     );
 };
 
--- src/components/StyledMovieContainer.js
+++ src/components/StyledMovieContainer.js
@@ -5,6 +5,7 @@ const StyledMMovieContainer = styled.section`
     background: ${props => props.theme.secondaryBg};
     width: 100%;
     height: 100vh;
+    padding: 1rem;
     
     ${devices.md`
         width: 640px;
