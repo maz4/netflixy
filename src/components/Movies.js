734e4005e6c29d677f58c727216a801b5e286989
Marcin <zasadzkimarcin@gmail.com>
2019-08-17 16:46:53 +0100

    build scroll list container and movie components

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import FontAwesomeIcon from './FontAwesomeIcon';
 import StyledHeader from './StyledHeader';
 import StyledHeaderTitle from './StyledHeaderTitle';
+import StyledHorizontalScroll from './StyledHorizontalScroll';
+import Movie from './Movie';
+import { movies } from '../staticData';
+
 
 const Movies = () => {
     return (
-        <StyledHeader>
-            <FontAwesomeIcon icon="bars" text="help" />
-            <StyledHeaderTitle>The Movie Recommender</StyledHeaderTitle>
-            <FontAwesomeIcon icon="search" />
-        </StyledHeader>
+        <Fragment>
+            <StyledHeader>
+                <FontAwesomeIcon icon="bars" text="help" />
+                <StyledHeaderTitle>The Movie Recommender</StyledHeaderTitle>
+                <FontAwesomeIcon icon="search" />
+            </StyledHeader>
+            <StyledHorizontalScroll>
+                {movies.map(movie => (
+                    <Movie
+                        key={movie.id}
+                        name={movie.name}
+                        poster={movie.poster}
+                        duration={movie.duration}
+                        year={movie.year}
+                    />
+                ))}
+            </StyledHorizontalScroll>
+        </Fragment>
     );
 };
 
---
+++ src/components/StyledHorizontalScroll.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledHorizontalScroll = styled.div`
+    display: flex;
+    flex: 1 1 0;
+    align-items: center;
+    flex-wrap: nowrap;
+    overflow: auto;
+`;
+
+export default StyledHorizontalScroll;
