0171086874f8ff57a21b489c7c34645d6a6a4688
Marcin <zasadzkimarcin@gmail.com>
2019-08-20 05:19:24 +0100

    update movie details component

--- src/components/StyledMovieTitle.js
+++ src/components/StyledMovieTitle.js
@@ -1,13 +1,13 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledMovieTitle = styled.div`
-    width: ${props => props.theme.posterWidth};
-    margin-bottom: 4px;
-    font-size: 1rem;
-    color: #525661;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    white-space: nowrap;
+  color: ${props => (props.isLarge ? "#ECF0F1" : "#525661")};
+  margin-bottom: 4px;
+  font-size: ${props => (props.isLarge ? "2.2rem" : "1rem")};
+  width: ${props => (props.isLarge ? "auto" : props.theme.posterWidth)};
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: ${props => (props.isLarge ? "wrap" : "nowrap")};
 `;
 
 export default StyledMovieTitle;
